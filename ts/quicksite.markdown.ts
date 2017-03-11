aaceaf6bed79601d488aaa742f43325c8723b7e7
Phil Kunz <gitlab@philkunz.com>
2017-03-11 22:18:14 +0100

    update CI

--- ts/quicksite.markdown.ts
+++ ts/quicksite.markdown.ts
@@ -1,7 +1,7 @@
 import * as plugins from './quicksite.plugins'
 
 // interfaces
-import { IQuicksiteFile } from './quicksite.interfaces' 
+import { IQuicksiteFile } from './quicksite.interfaces'
 
 export let markdownToHtml = async(quicksiteFiles: IQuicksiteFile[]) => {
   for (let quicksiteFile of quicksiteFiles) {
